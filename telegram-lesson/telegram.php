fafd9ab714696d1aec52560490dc8b1370ef6232
Eldosyano <123807714+Eldosyano@users.noreply.github.com>
2023-04-19 17:34:02 +0600

    Add files via upload

---
+++ telegram-lesson/telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/bot5997681515:AAHjVe8wE7ioTpCsfc6HNSDonqsLkDXvonw/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$token = "5997681515:AAHjVe8wE7ioTpCsfc6HNSDonqsLkDXvonw";
+$chat_id = "-1001967152858";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
